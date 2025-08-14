3a4f3717fb598bb4a29fb33cdb9bef8423ddf04e
¨Seva <sevakunjadas.bms@gmail.com>
2025-08-14 20:40:02 -0300

    todo: html

---
+++ js.js
@@ -0,0 +1,38 @@
+// const num1 = 
+// const num2 =
+// const operator =
+
+
+function add(a, b) {
+    return a + b;
+}
+
+
+function substract(a,b) {
+    return a - b;
+}
+
+
+function multiply(a, b) {
+    return a * b;
+}
+
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+
+function operate(operator, num1, num2) {
+
+    if (operator === '+') {
+        return add(num1 + num2);
+    } else if (operator === '-') {
+        return substract(num1, num2);
+    } else if(operator === '*') {
+        return multiply(num1 * num2);
+    } else if (operator === '/') {
+        return divide(num1 / num2);
+    } 
+}
